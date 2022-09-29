c9ad462815d8f9ad8c8bd29789cebf99e1bda33d
Sebastian Vergara <s_vergara@javeriana.edu.co>
2022-09-29 00:43:43 +0000

    Se actualiza el proyecto con lo desarrollado anteriormente a la creacion del git

---
+++ extractorConsola.h
@@ -0,0 +1,81 @@
+//
+// Created by svalp on 9/22/2022.
+//
+
+#ifndef UNTITLED_EXTRACTORCONSOLA_H
+#define UNTITLED_EXTRACTORCONSOLA_H
+
+// Clase que se encarga de extraer los datos de la consola
+// Ejemplo entrada por consola cliente $ cliente -i ID -p pipeNom
+// Ejemplo entrada por consola gestor $ gestor -n Num -r Relaciones -m modo -t time -p pipeNom
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct argumentos {
+    int id; ///< Identificador del cliente.
+    char *pipeNom; ///< Nombre del pipe nominal.
+    int num; ///< Es el número máximo de usuarios que se pueden registrar en el sistema. No todos estarán conectados al mismo tiempo. Es un número entero <= 100.
+    char* relaciones; ///< Es un archivo de texto que describe las relaciones actuales entre los usuarios del sistema. Las relaciones se describirán en un archivo de texto con una matriz de NumxNum. En cada línea del archivo se almacenará información sobre las relaciones de un usuario en particular. Dichas relaciones se expresan con números binarios, es decir, un cero (0) en una determinada posición significa que no se sigue a un determinado usuario y un uno (1) significa que sí sé es seguidor. (Estaestructura corresponde a la de una matriz de adyacencia de un grafo dirigido)
+
+    // Ejemplo de archivo de texto de N = 10
+    // 1 0 1 1 0 0 0 0 0 0 0
+    // 2 1 0 0 0 0 0 0 0 0 0
+    // 3 1 0 0 0 1 1 1 0 0 0
+    // 4 1 0 0 0 0 0 0 0 0 0
+    // 5 0 0 0 0 0 0 0 0 0 0
+    // 6 0 0 0 0 0 0 1 0 0 0
+    // 7 1 0 1 0 0 0 0 0 0 0
+    // 8 0 0 0 0 0 0 0 0 0 0
+    // 9 0 0 0 0 0 0 0 0 0 0
+    // 10 0 0 0 0 0 0 0 0 0 0
+    // La primera columna no es parte del archivo, se coloca para dejar claro que la primera fila
+    // corresponde a las relaciones del usuario 1, la segunda al usuario 2, etc. Este y otros archivos de
+    // ejemplo se colocarán en BS en la misma carpeta donde se coloque el proyecto.
+    // La gráfica de la figura 2 ilustra las relaciones de la matriz de la figura 1. Una flecha del nodo
+    // i al nodo j, indica que i es seguidor de j. Por ejemplo, en la gráfica, el usuario 4 es seguidor del
+    // usuario 1 (observar que hay un 1 en fila 4, primera columna). El usuario 2 sigue al usuario 1, y el
+    // usuario 1 sigue al usuario 2 y al usuario 3.
+
+    char modo; ///< indica si el modo en el que trabajará el Gestor es acoplado o desacoplado. Se usará el carácter A para el modo acoplado y el carácter D para el desacoplado (Ambos es mayúscula o minúscula)
+    int time; ///< Es un valor en segundos, que utilizará el Gestor para imprimir la siguiente información de forma periódica: Número de usuarios conectados, número total de tweets enviados y recibidos. Por ejemplo, si time = 2, cada 2 segundos el proceso Gestor imprimirá la información mencionada.
+};
+
+/// @brief Función que se encarga de extraer los datos de la consola
+/// @param argv Argumentos
+/// @param argc Cantidad de argumentos
+/// @return Estructura con los datos extraidos
+struct argumentos extraerDatosConsola(int argc, char *argv[])
+{
+    struct argumentos argumentos;
+    argumentos.id = 0;
+    argumentos.pipeNom = NULL;
+    argumentos.num = 0;
+    argumentos.relaciones = NULL;
+    argumentos.modo = ' ';
+    argumentos.time = 0;
+
+    int i;
+    for (i = 1; i < argc; i++) {
+        if (strcmp(argv[i], "-i") == 0) {
+            argumentos.id = atoi(argv[i + 1]);
+        } else if (strcmp(argv[i], "-p") == 0) {
+            argumentos.pipeNom = argv[i + 1];
+        } else if (strcmp(argv[i], "-n") == 0) {
+            argumentos.num = atoi(argv[i + 1]);
+        } else if (strcmp(argv[i], "-r") == 0) {
+            argumentos.relaciones = argv[i + 1];
+        } else if (strcmp(argv[i], "-m") == 0) {
+            argumentos.modo = argv[i + 1][0];
+        } else if (strcmp(argv[i], "-t") == 0) {
+            argumentos.time = atoi(argv[i + 1]);
+        }
+    }
+
+    return argumentos;
+}
+
+
+
+#endif //UNTITLED_EXTRACTORCONSOLA_H
---
+++ gestor.c
@@ -0,0 +1,89 @@
+// Proyecto entreaga 1: Sistemas Operativos
+// Sebastian Vergara
+// TODO: Inicialización del proceso Gestor con los parámetros correspondientes.
+//  El gestor crea las estructuras de datos necesarias y muestra los usuarios registrados.
+
+// PROGRAMA DEL PROCESO GESTOR
+
+// El Gestor es el primer programa que se debe invocar. Éste crea e inicializa las estructuras de datos
+// que permiten almacenar a los usuarios y sus relaciones. También crea el primer pipe nominal por
+// donde se recibirá la información y solicitudes de los usuarios que se van conectando (Ver figura 3,
+// Izquierda). El Gestor nunca deja de funcionar a menos que se elimine de forma explícita desde el
+// terminal.
+
+// El Gestor tiene dos tareas principales:
+// a) Atender las solicitudes de los procesos Cliente: registro de usuarios, follow, unfollow, enviar un tweet y desconexión y
+// b) Imprimir estadísticas
+
+// Es el proceso encargado de gestionar el envío de tweets entre usuarios y, por lo tanto, de almacenar
+// los datos correspondientes; también almacena las relaciones existentes entre ellos. El Gestor va a
+// tener dos modos de operación: acoplado y desacoplado. En el modo desacoplado, los usuarios
+// reciben los tweets que les han enviado, únicamente al conectarse al sistema y cuando
+// explícitamente solicitan una actualización de sus tweets. En el modo acoplado,se reciben los tweets
+// luego del momento de conexión y en el mismo instante en el que son enviados por otros usuarios
+// (a quienes ellos siguen), es decir, en este modo, si un usuario A envía un tweet en un momento
+// determinado, sus seguidores conectados deben recibirlo inmediatamente.
+
+// Como compilar: gcc -o Gestor Gestor.c -lpthread
+// Como ejecutar: ./Gestor -n 10 -m 10 -r 1 -t 10 -p 10
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <pthread.h>
+#include "extractorConsola.h" // Funciones para extraer datos de la consola
+
+// Registro: Al momento de conectarse un usuario, el proceso Cliente enviará el identificador
+// del usuario al Gestor usando el pipe. El Gestor le devolverá al Cliente el resultado de la
+// operación (si fue exitosa o no), lo cual dependerá fundamentalmente de si un usuario con
+// el mismo identificador ya se encuentra conectado en el sistema o si el número esta fuera
+// del rango especificado por Num. También, si es el caso, le enviará los tweets que tiene
+// almacenado para ese usuario. Si Ud. desea crear otro pipe para la comunicación exclusiva
+// entre el Gestor y el Cliente (Figura 3, derecha pipes rojos), puede aprovechar la operación
+// de registro para crear e intercambiar la información para este nuevo pipe.
+
+struct argumentos args; // Estructura para almacenar los argumentos de la consola
+
+/// @brief Funcion para imprimir estadisticas
+void *imprimirEstadisticas(void *arg){
+    printf("Imprimiendo estadisticas\n");
+    return NULL;
+}
+
+/// @brief Funcion para atención de solicitudes de los procesos Cliente
+void *atenderSolicitudes(void *arg){
+    printf("Hola mundo");
+    return NULL;
+}
+
+/// @brief Funcion principal del proceso Gestor
+int main(int argc, char *argv[]) {
+    // Inicializar el gestor
+
+    // Extraer los datos de la consola¿
+    args = extraerDatosConsola(argc, argv);
+
+    // Crear el primer pipe nominal con mkfifo
+    mkfifo("pipeNominal", 0666);
+
+    // Crear el hilo para atender solicitudes
+    pthread_t hiloSolicitudes;
+    pthread_create(&hiloSolicitudes, NULL, atenderSolicitudes, NULL);
+
+    // Crear el hilo para imprimir estadisticas
+    pthread_t hiloEstadisticas;
+    pthread_create(&hiloEstadisticas, NULL, imprimirEstadisticas, NULL);
+
+    // Esperar a que los hilos terminen
+    pthread_join(hiloSolicitudes, NULL);
+    pthread_join(hiloEstadisticas, NULL);
+
+    return 0;
+}
+
+
+
+
